8b3706a52f3cbca9207539c8e6a126f70ed4e3f8
valentinstukov <tinych2@gmail.com>
2024-10-05 16:04:39 +0400

    fix:add h1 in app.components.ts

--- landpage/src/app/app.component.ts
+++ landpage/src/app/app.component.ts
@@ -4,10 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [],
+  template: `
+    <h1>Resume</h1>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'landpage';
+  title = 'simple landpage';
 }
